97e2080ced5033d799b8d77edf419f9ad9bc0947
Abhijeet Kumar <196628277+Abhijeetkr2706@users.noreply.github.com>
2026-03-23 19:17:42 +0530

    WIP: saving local changes before pull

--- Backend/server.js
+++ Backend/server.js
@@ -4,13 +4,14 @@
 //start server
 
 const app=require("./app");
-
-
+const connectDatabase=require('./db')
 //import dotenv
 const dotenv=require("dotenv");
 dotenv.config({path: "./config/config.env"});
 
+connectDatabase();
 //start the server
+
 
 const server=app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
