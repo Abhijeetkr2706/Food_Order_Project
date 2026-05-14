4d186f092d1235d25621355752607d25b0bd624b
Abhijeet Kumar <abhijeetkr2527@gmail.com>
2026-05-14 19:22:15 +0530

    Update catchAsyncErrors.js

--- Backend/middleware/catchAsyncErrors.js
+++ Backend/middleware/catchAsyncErrors.js
@@ -2,11 +2,5 @@
 
 
 module.exports = (func) => (req, res, next) =>
-
-  // Promise.resolve ensures that the function runs as a promise
-  // so that any error inside async code can be caught
   Promise.resolve(func(req, res, next))
-
-    // If any error happens, it will be passed to Express error middleware
-    // using next(error)
-    .catch(next);
+  .catch(next);
