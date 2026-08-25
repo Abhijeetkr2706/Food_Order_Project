be5ad4311191518c59bc880da11d7e31d3014f01
Abhijeet Kumar <abhijeetkr2527@gmail.com>
2026-08-25 22:19:19 +0530

    Update errors.js

--- Backend/middleware/errors.js
+++ Backend/middleware/errors.js
@@ -34,8 +34,6 @@ module.exports = (err, req, res, next) => {
 
       error = new ErrorHandler(message, 400);
     }
-
-    // Example: user tries to register with an email that already exists
     if (err.code === 11000) {
 
       const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
